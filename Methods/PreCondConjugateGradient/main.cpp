357c1e23d4aa440c3ad5a7e393b91148de50ee8d
fernandezdlg <juanfernandezdlg@gmail.com>
2023-10-01 03:40:52 +0200

    added doxygen

--- Methods/PreCondConjugateGradient/main.cpp
+++ Methods/PreCondConjugateGradient/main.cpp
@@ -7,8 +7,15 @@ using std::cout;
 using std::endl;
 
 
-// Explicit implementation of a preconditioned conjugate gradient method (probably suboptimal)
-VectorXf precondConjGrad(const MatrixXf& Minv, const MatrixXf& A, const VectorXf& b, const VectorXf& x0, const float& tol, const u_int& maxIter)
+/// @brief Computes the solution of the linear system Ax = b using the preconditioned conjugate gradient method. The matrices A and M are expected to be symmetric and positive definite (SPD).
+/// @param Minv is the inverse of the preconditioner matrix.
+/// @param A is the matrix of the linear system.
+/// @param b is the right hand side of the linear system.
+/// @param x0 is the initial guess of the solution.
+/// @param tol is the error tolerance of the method.
+/// @param maxIter is the maximum number of iterations.
+/// @return Estimation of the solution x.
+VectorXf precondConjugateGradient(const MatrixXf& Minv, const MatrixXf& A, const VectorXf& b, const VectorXf& x0, const float& tol, const u_int& maxIter)
 {
     const float err = 1e-14 * b.norm();
 
@@ -55,7 +62,7 @@ int main()  // int argc, char* argv[])
     float tol = 1e-14 * b.norm();
     u_int maxIter = 10000;
 
-    VectorXf x = precondConjGrad(Minv, A, b, x0, tol, maxIter);
+    VectorXf x = precondConjugateGradient(Minv, A, b, x0, tol, maxIter);
     cout << "This is the initial vector x0 : " << endl;
     cout << x0 << endl;
     cout << "This is the found solution vector x : " << endl;
